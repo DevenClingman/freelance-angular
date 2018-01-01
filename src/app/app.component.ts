6fd760a281446119b83c9083647635e8b85a4438
Deven Clingman <dclingy@gmail.com>
2018-01-01 16:06:02 -0700

    initial commit and added routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  moduleId: module.id,
+  selector: 'app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+
+  <h1>Navigation</h1>
+
+
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Freelance Dashboard';
 }
