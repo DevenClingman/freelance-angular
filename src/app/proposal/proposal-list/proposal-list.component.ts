ce1a2d3de0fb0c2c1b21e0967ecaf7672b900ed3
Deven Clingman <dclingy@gmail.com>
2018-01-02 16:43:40 -0700

    created mock data for proposal list and added it to proposal list index page

--- src/app/proposal/proposal-list/proposal-list.component.ts
+++ src/app/proposal/proposal-list/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Proposal } from '../proposal';
 
 @Component({
   selector: 'proposal-list',
@@ -6,9 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./proposal-list.component.css']
 })
 export class ProposalListComponent{
+  proposalOne: Proposal = new Proposal(15, "xyz company", 'http://devenclingman.com', 'Ruby on Rails', 150, 120, 15, 'dclingy@gmail.com');
 
-  constructor() { }
+  proposalTwo: Proposal = new Proposal(16, "Yahoo", 'http://devenclingman.com', 'Ruby on Rails', 150, 120, 15, 'dclingy@gmail.com');
 
- 
+  proposalThree: Proposal = new Proposal(17, "Google", 'http://devenclingman.com', 'Ruby on Rails', 150, 120, 15, 'dclingy@gmail.com');
 
+  proposals: Proposal[] = [this.proposalOne, this.proposalTwo, this.proposalThree]
 }
