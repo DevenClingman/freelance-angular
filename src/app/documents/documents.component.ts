c547b9a86d1373d5e5f55f06389aae1eb00958f5
Deven Clingman <dclingy@gmail.com>
2018-01-02 12:54:39 -0700

    created mock data for documents

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   selector: 'documents',
@@ -6,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent implements OnInit {
+
+  documents: Document[] = [
+    {
+      title: "My First Doc",
+      description: "asdfjldksjflds",
+      file_url: "http://google.com",
+      updated_at: "11/11/16",
+      image_url: "http://google.com"
+    },
+    {
+      title: "My Second Doc",
+      description: "asdfjldksjflds",
+      file_url: "http://google.com",
+      updated_at: "11/11/16",
+      image_url: "http://google.com"
+    },
+    {
+      title: "My Last Doc",
+      description: "asdfjldksjflds",
+      file_url: "http://google.com",
+      updated_at: "11/11/16",
+      image_url: "http://google.com"
+    },
+  ]
 
   constructor() { }
 
