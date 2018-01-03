13bcf948a24d8e54b36b8bb233aa2c71cf33dd8a
Deven Clingman <dclingy@gmail.com>
2018-01-03 14:38:18 -0700

    created proposal-new component and added to nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new/proposal-new.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ProposalListComponent } from './proposal/proposal-list/proposal-list.component';
+
+
 
 
 
@@ -17,6 +20,7 @@ import { ProposalListComponent } from './proposal/proposal-list/proposal-list.co
     HomepageComponent,
     DocumentsComponent,
     ProposalListComponent,
+    ProposalNewComponent
   ],
   imports: [
     BrowserModule,
